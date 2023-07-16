e1c79e389cf2b89f80243a1fd5b1f7872492f63d
Everton Souza <evrasouza@gmail.com>
2023-07-16 18:43:17 -0300

    add update runner class

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,8 +1,5 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import tests.car.*;
-import tests.login.*;
-import tests.product.*;
 import tests.users.*;
 
 @RunWith(Suite.class)
